ea7f26d8b4a43f1a598a3537e5a5b2274fd0500e
eduardo Alves <eduardo.telecomrbs@gmail.com>
2023-09-18 23:09:38 -0300

    Envio_de_teste

---
+++ Api.py
@@ -0,0 +1,32 @@
+import pyodbc
+
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-Q3JLRD9\SQLSERVER2023;"
+    "Database=GrammysDB;"
+)
+
+conexao = pyodbc.connect(dados_conexao)
+print('conexão realizada com sucesso')
+
+cursor = conexao.cursor()
+
+comando = """ SELECT*  FROM Person.Person """;
+
+
+cursor.execute('''
+  SELECT artist, MAX(sales) AS ARTISTA_MAIOR_FATURAMENTO
+FROM [money-makers-bb]
+GROUP BY artist
+ORDER BY ARTISTA_MAIOR_FATURAMENTO DESC
+''')
+
+# Recuperar os resultados
+resultados = cursor.fetchall()
+
+# Imprimir os resultados
+for item, visualizacoes in resultados:
+    print(f'Item: {item}, Visualizações: {visualizacoes}')
+
+# Fechar a conexão com o banco de dados
+conexao.close()
